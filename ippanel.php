89dc37184de866a1eca72b1df7b5b24d53fbe22b
Max Base <MaxBaseCode@Gmail.Com>
2021-09-21 19:16:25 +0430

    Create ippanel.php

---
+++ ippanel.php
@@ -0,0 +1,47 @@
+<?php
+class SmsIppanel {
+	private $key_id = '';
+	private $pattern_id = '';
+	private $number = '';
+	private $url = "https://ippanel.com/services.jspd";
+
+	public setKey($key) {
+		$this->key_id = $key;
+	}
+
+	public setPattern($pattern) {
+		$this->pattern_id = $pattern;
+	}
+
+	public setNumber($number) {
+		$this->number = $number;
+	}
+
+	public function sendPattern($to, $code) {
+		$_params = [
+			'apikey'=>$this->key_id,
+			'pid'=>$this->pattern_id,
+
+			'fnum'=>$this->number,
+			'tnum'=>$to,
+
+			'p1'=>'verification',
+			'v1'=>$code,
+		];
+		// print_r($_params);
+
+		$params = '';
+		foreach($_params as $key=>$value) {
+			$params .= $key."=".$value."&"; 
+		}
+		$params = rtrim($params, "&");
+
+		$handler = curl_init($this->url);
+		curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "GET");
+		curl_setopt($handler, CURLOPT_POSTFIELDS, $params);
+		curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($handler);
+
+		return $response;
+	}
+}
