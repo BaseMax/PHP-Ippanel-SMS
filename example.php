d15d7ddaa2de2a6e4a035929b0142712b1767110
Max Base <MaxBaseCode@Gmail.Com>
2021-09-21 19:18:38 +0430

    Update example.php

--- example.php
+++ example.php
@@ -5,12 +5,12 @@ require "ippanel.php";
 // Define the class
 $sms = new SmsIppanel();
 // Set the Key
-$sms->setKey('ROlRLxim-RlHf1RpXrHim6oo8y2sROF1ISlSsOBIh1Y=');
+$sms->setKey('ROlxxxxxim-xxxx=');
 // Set the Pattern Key
-$sms->setPattern('d2re63ztr2');
+$sms->setPattern('d2re6xxxxx');
 // Set phonennumber sender
 $sms->setNumber('9890000145');
 // Pass target phone number and the verification code
-$res = $sms->sendPattern('09134950787', rand(1000, 9999));
+$res = $sms->sendPattern('0912xxxxxxx', rand(1000, 9999));
 
 var_dump($res);
